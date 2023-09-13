239f9c357d2f404018f6df2a4aa464be3e508cc1
Jeremy Delley <jeremy.delley@imex-dtp.com>
2023-09-13 22:35:37 +0200

    Initial commit

---
+++ episodus.py
@@ -0,0 +1,309 @@
+from os.path import isdir
+from os import path
+import re
+import shutil
+from langcodes import Language
+from langcodes import standardize_tag
+from pyarr import SonarrAPI
+import json
+import os
+import subprocess
+
+
+SONARR_HOST_URL = 'http://10.100.3.2:8989'
+SONARR_API = '6339d80ef2354a8dbdf3ce8fd4528d4d'
+TEMP_FOLDER = "./temp/"
+
+
+class Episode():
+    def __init__(self):
+        self._number = ""
+        self._season = ""
+        self._video_path = ""
+        self.serie_path = ""
+        self._sonarr_var = None
+        self.release = ""
+        self.tvdbid = ""
+        self._copy_temp_path = ""
+        self._temp_folder = TEMP_FOLDER
+
+    @property
+    def temp_path(self) -> str:
+        return self._copy_temp_path
+
+    @property
+    def video_path(self) -> str:
+        return self._video_path
+
+    @video_path.setter
+    def video_path(self, new_path: str) -> None:
+        self._video_path = new_path
+
+    @property
+    def number(self) -> str:
+        return self._number
+
+    @number.setter
+    def number(self, value: str | int) -> None:
+        try:
+            number = int(value)
+            formated_number = f"{number:02d}"
+            self._number = formated_number
+        except ValueError:
+            print("Episode number is not a valid format")
+
+    @property
+    def season(self) -> str:
+        return self._season
+
+    @season.setter
+    def season(self, value: str | int) -> None:
+        try:
+            season = int(value)
+            formated_season = f"{season:02d}"
+            self._season = formated_season
+        except ValueError:
+            print("Season number is not a valid format")
+
+    @property
+    def sonarr_var(self):
+        return self._sonarr_var
+
+    @sonarr_var.setter
+    def sonarr_var(self, value):
+        self._sonarr_var = value
+        self._video_path = value.get("sonarr_episodefile_path")
+        self.tvdbid = value.get("sonarr_series_tvdbid")
+        self.number = value.get("sonarr_episodefile_episodenumbers")
+        self.season = value.get("sonarr_episodefile_seasonnumber")
+        self.serie_path = value.get("sonarr_series_path")
+        self.release = value.get("sonarr_episodefile_releasegroup")
+
+    def copy_temp(self) -> str:
+        path = shutil.copy(self._video_path, self._temp_folder)
+        self._copy_temp_path = path
+        return path
+
+    def delete_temp(self) -> None:
+        temp_folder = self._temp_folder
+        if os.path.exists(temp_folder) and os.path.isdir(temp_folder):
+            files = os.listdir(temp_folder)
+            if files:
+                for file in files:
+                    file_path = os.path.join(temp_folder, file)
+                    if os.path.isfile(file_path):
+                        os.remove(file_path)
+
+
+class Tracks():
+    def __init__(self):
+        self._audio = []
+        self.__subs = []
+        self._tracks = {}
+        self._video_path = ""
+
+    @property
+    def subs(self) -> list:
+        return self.__subs
+
+    @property
+    def too_big(self) -> bool:
+        size_in_bytes = os.path.getsize(self._video_path)
+        size_in_mb = int(size_in_bytes / (1024 * 1024))
+        track_number = len(self.__subs)
+        return size_in_mb > 400 or track_number > 2
+
+    def analyze(self, json_data: dict = {}, video_path: str = "") -> None:
+        self.__subs = []
+        if not json_data:
+            json_data = self._tracks
+        if video_path == "":
+            video_path = self._video_path
+        if (json_data.get("tracks") is not None):
+            for track in json_data["tracks"]:
+                default = ""
+                if track["type"] == "subtitles":
+                    track_props = track["properties"]
+                    sub_type = track_props.get("codec_id")
+                    sub_type_extention = self.extension(sub_type)
+                    track_id = track["id"]
+                    is_forced = track_props.get("forced_track")
+                    if is_forced:
+                        forced_flag_txt = "forced."
+                    else:
+                        forced_flag_txt = ""
+                    track_name = track_props.get("track_name", "und")
+                    track_name = track_name.replace(r"/", "#")
+                    if track_name != "und" and not is_forced:
+                        if "signs" in track_name.lower():
+                            is_forced = True
+                            forced_flag_txt = "forced."
+                        elif "songs" in track_name.lower():
+                            is_forced = True
+                            forced_flag_txt = "forced."
+                        elif "forc" in track_name.lower():
+                            is_forced = True
+                            forced_flag_txt = "forced."
+                    track_lang_ietf = track_props.get("language_ietf", "und")
+                    track_lang = track_props.get("language", "und")
+                    if track_lang == "und":
+                        track_lang = self.guess_lang(track_name)
+                    if track_lang == "und" or not track_lang:
+                        track_lang = self.guess_lang_harder(video_path,
+                                                            track_id,
+                                                            sub_type_extention)
+                    if track_lang != "und" and track_name == "und":
+                        track_name = (f"{Language.get(track_lang).display_name()} # "
+                                      f"{Language.get(track_lang).display_name(track_lang)}")
+                    if track_lang != "und":
+                        if "fr" in track_lang:
+                            default = "default."
+                    if track_lang_ietf != 'und':
+                        track_lang = track_lang_ietf
+                    else:
+                        track_lang = standardize_tag(track_lang)
+                    track_info = {"track_id": track_id,
+                                  "sub_type_extention": sub_type_extention,
+                                  "track_name": track_name,
+                                  "forced_flag_txt": forced_flag_txt,
+                                  "track_lang": track_lang,
+                                  "default": default}
+                    self.__subs.append(track_info)
+
+    def identify(self, video_file: str) -> bool:
+        json_data = ""
+        cmd = [f"mkvmerge -i -J \"{video_file}\""]
+        print(cmd)
+        proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        json_data, err = proc.communicate()
+        json_data = json_data.decode("utf-8")
+        json_data = json.loads(json_data)
+        self._video_path = video_file
+        self._tracks = json_data
+        return not json_data["errors"]
+
+    def normalize_lang(self, track: dict, trackname="") -> dict:
+        do_something = track
+        return do_something
+
+    def extension(self, sub_type) -> str:
+        sub_extension = ""
+        if "PGS" in sub_type:
+            sub_extension = "sup"
+        elif "ASS" in sub_type:
+            sub_extension = "ass"
+        elif "SSA" in sub_type:
+            sub_extension = "ssa"
+        elif "UTF8" in sub_type or "ASCII" in sub_type:
+            sub_extension = "srt"
+        elif "VOBSUB" in sub_type:
+            sub_extension = "sub"
+        elif "USF" in sub_type:
+            sub_extension = "usf"
+        elif "WEBVTT" in sub_type:
+            sub_extension = "vtt"
+        return sub_extension
+
+    def guess_lang(self, track_name):
+        try:
+            track_lang = Language.find(track_name)
+            return track_lang
+        except LookupError:
+            print("Unable to determine language for subtitle track")
+            return False
+
+    def guess_lang_harder(self, video_file, track_id, sub_extention):
+        result = ""
+        if sub_extention:
+            sub_path = self.export(video_file, track_id,
+                                   "./caca" + sub_extention)
+        return result
+
+    def read_ass_sub(self):
+        pass
+
+    def read_ssa_sub(self):
+        pass
+
+    def read_srt_sub(self):
+        pass
+
+    def export(self, video_file: str, track_id: str | int, path: str) -> str:
+        cmd = [
+            f"mkvextract tracks \"{video_file}\" {str(track_id)}:\"{path}\""]
+        print(cmd)
+        subprocess.run(cmd, shell=True, check=True)
+        return path
+
+
+class Sonarr():
+    def __init__(self) -> None:
+        self._sonarr = SonarrAPI(SONARR_HOST_URL, SONARR_API)
+        self._series = []
+        self._episode_list = []
+        self._episode_path = []
+
+    # tvdbid, seasonnumber, episodenumber, releasegroup
+
+    @property
+    def series(self):
+        series = self._sonarr.get_series()
+        self._series = series
+        return self._series
+
+    def episode_list(self, serie_id):
+        ep_list = self._sonarr.get_episode_file(serie_id, series=True)
+        self._episode_list = ep_list
+        return self._episode_list
+
+
+class Subtitles(Tracks):
+    def __init__(self) -> None:
+        self.__subs_list = []
+
+    @property
+    def subs_list(self):
+        return self.__subs_list
+
+    def analyze_folder(self, folder_path: str) -> list:
+        if isdir(folder_path):
+            all_subs = os.listdir(folder_path)
+            if len(all_subs) > 0:
+                for subtitle in all_subs:
+                    full_path = path.join(folder_path, subtitle)
+                    subtitle_track = self.subtitle_info_from_file(full_path)
+                    self.__subs_list.append(subtitle_track)
+        return self.__subs_list
+
+    def subtitle_info_from_file(self, file_path: str) -> dict:
+        base_dir = path.dirname(file_path)
+        file_name = path.basename(file_path)
+        is_default = False
+        is_forced = False
+        match_pattern = r'S(\d{2,3})\.E(\d{2,3})\.\[(.+)\]-\[(.+)\]\.(.+)\.(\w{3}$)'
+        # file_path = 'S01.E20.[Retr0]-[Signs#Songs [Commie]].default.eng.forced.ass'
+        re_match = re.search(string=file_name, pattern=match_pattern)
+        season = re_match.group(1)
+        episode = re_match.group(2)
+        rel_group = re_match.group(3)
+        trackname = re_match.group(4)
+        flags = re_match.group(5)
+        sub_extention = re_match.group(6)
+        if 'default' in flags:
+            is_default = True
+            flags = flags.replace('default.', '')
+        if 'forced' in flags:
+            is_forced = True
+            flags = flags.replace('forced', '')
+        flags = flags.replace('.', '')
+        lang_ = standardize_tag(flags)
+        sub = {'base_dir': base_dir,
+               'file_name': file_name,
+               'default_flag': is_default,
+               'forced_flag': is_forced,
+               'season': season,
+               'episode': episode,
+               'trackname': f'[{rel_group}]-[{trackname}]',
+               'subtype': sub_extention,
+               'language_ietf': lang_}
+        return sub
---
+++ main.py
@@ -0,0 +1,159 @@
+import os
+import re
+import argparse
+# import sys
+# import subprocess
+# import py3langid as langid
+# from iso639 import Lang
+# from langcodes import Language
+# from langcodes import standardize_tag
+# from pysubparser import parser
+# import ass
+# from ass_tag_parser import parse_ass
+from episodus import Episode
+from episodus import Tracks
+from episodus import Sonarr
+from episodus import Subtitles
+
+GRABING_FOLDER = "./grabs/"
+SUBTITLE_PATH = "/home/monheim/Documents/subtitles/"
+PROGRESS_FOLDER = './progress/current.txt'
+
+
+def episode_num_from_path(path: str) -> int:
+    pattern = r'S\d{2}E(\d{2})'
+    re_match = re.search(pattern, path)
+    episode_num = int(re_match.group(1))  # pyright: ignore
+    return episode_num
+
+
+def export_all_from_sonarr():
+    sonarr = Sonarr()
+    all_series = sonarr.series
+    for serie in all_series:
+        serie_id = serie.get("id")
+        serie_tvid = serie.get("tvdbId")
+        ep_list = sonarr.episode_list(serie_id)
+        already_done = read_progress_sonarr()
+        if str(serie_id) not in already_done:
+            for episode in ep_list:
+                ep_path = episode.get("path")
+                season_num = f"{episode.get('seasonNumber'):02d}"
+                release = episode.get('releaseGroup')
+                ep_num = f"{episode_num_from_path(ep_path):02d}"
+                export_ep(ep_path, serie_tvid, ep_num, season_num, release)
+        save_progress_sonarr(serie_id)
+
+
+def export_ep(ep_path: str,
+              tvid: str,
+              ep_num: str,
+              season: str,
+              rel_group: str) -> None:
+    tracks = Tracks()
+    ep = Episode()
+    ep.video_path = ep_path
+    if tracks.identify(ep_path):
+        tracks.analyze()
+        if tracks.too_big:
+            video_path = ep.copy_temp()
+        else:
+            video_path = ep.video_path
+        for track in tracks.subs:
+            sub_path_full = (f"{SUBTITLE_PATH}"
+                             f"{tvid}/S{season}/E{ep_num}/"
+                             f"S{season}.E{ep_num}."
+                             f"[{rel_group}]-[{track.get('track_name')}]."
+                             f"{track.get('default')}"
+                             f"{track.get('track_lang')}."
+                             f"{track.get('forced_flag_txt')}"
+                             f"{track.get('sub_type_extention')}")
+            subs = Subtitles()
+            subs.analyze_folder(os.path.dirname(sub_path_full))
+            # compare external subs to new video
+            # if lang exist in new video flag as no merge
+            tracks.export(video_path, track.get("track_id"), sub_path_full)
+            # if not args.all or args.reset, only on standard queue
+            # eport temp subtitle track for syncronization with ffsubsync
+            # remux prev external subs with new video and try to sync new subs
+        ep.delete_temp()
+
+
+def get_sonarr_var(data_file_path):
+    data_from_file = {}
+    with open(data_file_path, "r") as file:
+        for line in file:
+            line = line.strip()
+            if line.startswith("#"):
+                continue  # Skip comments
+            key, value = line.split("=", 1)
+            data_from_file[key] = value
+    return data_from_file
+
+
+def treat_queue_from_sonarr(source_folder) -> None:
+    # Get a list of all files in the source folder
+    ep = Episode()
+    files = os.listdir(source_folder)
+    for file in files:
+        file_path_full = os.path.join(source_folder, file)
+        ep.sonarr_var = get_sonarr_var(file_path_full)
+        export_ep(ep.video_path, ep.tvdbid, ep.number, ep.season, ep.release)
+        os.remove(file_path_full)
+
+
+def save_progress_sonarr(serie_id: int | str) -> None:
+    try:
+        already_done = read_progress_sonarr()
+        with open(PROGRESS_FOLDER, 'a+') as file:
+            if str(serie_id) not in already_done:
+                file.write(f'{str(serie_id)};')
+    except Exception as e:
+        print(f'An error occured: {e}')
+
+
+def read_progress_sonarr() -> list:
+    try:
+        with open(PROGRESS_FOLDER, 'r') as file:
+            series = file.read()
+            serie_ids = [serie.strip()
+                         for serie in series.split(';')
+                         if serie.strip()]
+            return serie_ids
+    except FileNotFoundError:
+        return []
+    except Exception as e:
+        print(f'An error occured: {e}')
+        return []
+
+
+def reset_progress_sonarr() -> None:
+    with open(PROGRESS_FOLDER, 'w') as file:
+        file.flush()
+        pass
+
+
+def main():
+    arg = argparse.ArgumentParser(
+        description='Sonarr Subtitle (Auto)Managerr')
+    arg.add_argument('-a', '--all', action='store_true',
+                     help='Export all episode from Sonarr')
+    arg.add_argument(
+        '-r', '--reset', action='store_true', help='Reset from the benginginn')
+    arg.add_argument('-g', '--grabs', action='store_true',
+                     help='Treat queue from the grabing folder')
+    args = arg.parse_args()
+    if args.reset:
+        reset_progress_sonarr()
+        export_all_from_sonarr()
+    if args.all and not args.reset:
+        export_all_from_sonarr()
+    if args.grabs:
+        treat_queue_from_sonarr(GRABING_FOLDER)
+    if not any(vars(args).values()):
+        treat_queue_from_sonarr(GRABING_FOLDER)
+
+
+if __name__ == "__main__":
+    main()
+    # tvdbid, seasonnumber, episodenumber, releasegroup
