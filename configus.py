2b463bffe1ebb14e370cbae41a347015662220c2
Jeremy Delley <jeremy.delley@imex-dtp.com>
2023-09-14 18:02:24 +0200

    create config file
    Yeah... I mean, pretty obvious isn't it?

---
+++ configus.py
@@ -0,0 +1,6 @@
+CONF_SONARR_HOST_URL = 'http://0.0.0.0:8989'
+CONF_SONARR_API = 'somethinapiblabla'
+CONF_TEMP_FOLDER = './temp/'
+CONF_GRABING_FOLDER = "./grabs/"
+CONF_SUBTITLE_PATH = "~/Documents/subtitles/"
+CONF_PROGRESS_FOLDER = './progress/current.txt'
